99ca618a0d3be7235202941b358c3c813759f65f
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-10 22:38:05 +0530

    Create network-firewall.tf

---
+++ network-firewall.tf
@@ -0,0 +1,40 @@
+# allow http traffic
+resource "google_compute_firewall" "allow-http" {
+  name = "${var.app_name}-fw-allow-http"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  target_tags = ["http"]
+}
+# allow https traffic
+resource "google_compute_firewall" "allow-https" {
+  name = "${var.app_name}-fw-allow-https"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  target_tags = ["https"]
+}
+# allow ssh traffic
+resource "google_compute_firewall" "allow-ssh" {
+  name = "${var.app_name}-fw-allow-ssh"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  target_tags = ["ssh"]
+}
+# allow rdp traffic
+resource "google_compute_firewall" "allow-rdp" {
+  name = "${var.app_name}-fw-allow-rdp"
+  network = "${google_compute_network.vpc.name}"
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+  target_tags = ["rdp"]
+}
