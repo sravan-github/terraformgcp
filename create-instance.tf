2eee54541a1a1649694aeb3da214fb7be79a60d7
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-10 22:31:32 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,42 +1,26 @@
-resource "google_compute_instance" "default" {
-  name         = "virtual-machine-from-terraform"
+# Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+  byte_length = 4
+}
+# Create VM #1
+resource "google_compute_instance" "vm_instance_public" {
+  name = "${var.app_name}-vm-${random_id.instance_id.hex}"
   machine_type = "f1-micro"
-  zone         = "us-central1-a"
-
+  zone = var.gcp_zone_1
+  hostname = "${var.app_name}vm-${random_id.instance_id.hex}.${var.app_domain}"
+  tags = ["ssh","http"]
+  
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
-
+  metadata_startup_script = "sudo apt-get update; 
+    sudo apt-get install -yq build-essential apache2"
   network_interface {
-    network = "default"
-
-    access_config {
-      // Include this section to give the VM an external ip address
-    }
+    network = google_compute_network.vpc.name
+    subnetwork = google_compute_subnetwork.public_subnet_1.name
+  
+  access_config { }
   }
-
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo Hello world from $(hostname)$(hostname -I) > /var/www/html/index.html"
-
-    // Apply the firewall rule to allow external IPs to access this instance
-    tags = ["http-server"]
-}
-
-resource "google_compute_firewall" "http-server" {
-  name    = "default-allow-http-terraform"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  // Allow traffic from everywhere to instances with an http-server tag
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
-}
-
-output "ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
