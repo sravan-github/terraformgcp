2783ec11d947c2ff705bee7c1ff3b2c008677f74
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-10 22:37:11 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,3 +4,11 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = "false" 
   routing_mode = "GLOBAL"
 }
+
+# create public subnet
+resource "google_compute_subnetwork" "public_subnet_1" {
+  name = "${var.app_name}-public-subnet-1"
+  ip_cidr_range = var.public_subnet_cidr_1
+  network = google_compute_network.vpc.name
+  region = var.gcp_region_1
+}
