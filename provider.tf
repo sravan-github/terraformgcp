9f2d8ec34898c44d2e0801f9c0d7fe48ff05f94c
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-10 21:30:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("/var/lib/jenkins/workspace/gcp-vm/key.json")}"
+#credentials = "${file("/var/lib/jenkins/workspace/gcp-vm/key.json")}"
 project = "web-project-333613"
 region = "us-central1"
 }
