db6d09f8f5b5a2007a983245bacc7af5e763fd03
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-10 22:29:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,10 @@
-# Specify the provider (GCP, AWS, Azure)
+# setup the GCP provider
+terraform {
+  required_version = ">= 0.12"
+}
 provider "google" {
-#credentials = "${file("/var/lib/jenkins/workspace/gcp-vm/key.json")}"
-project = "dolphine-project"
-region = "us-central1"
+  project     = "	dolphine-project"
+  credentials = file("/home/sravangcp/gcp-key.json")
+  region      = "europe-west1"
+  zone        = "europe-west1-b"
 }
