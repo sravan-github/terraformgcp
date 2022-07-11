4bf9da9ab0eaa2c0a959f71ebf4843dfcd780efb
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-11 18:18:21 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -5,7 +5,8 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      //image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
 
