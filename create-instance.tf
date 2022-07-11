d47c95961a5b7f0e8b38e60a18dfc2739a7c0d75
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-11 15:54:24 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,25 +1,42 @@
-# Terraform plugin for creating random ids
-resource "random_id" "instance_id" {
-  byte_length = 4
-}
-# Create VM #1
-resource "google_compute_instance" "vm_instance_public" {
-  name = "${var.app_name}-vm-${random_id.instance_id.hex}"
+resource "google_compute_instance" "default" {
+  name         = "virtual-machine-from-terraform"
   machine_type = "f1-micro"
-  zone = var.gcp_zone_1
-  hostname = "${var.app_name}vm-${random_id.instance_id.hex}.${var.app_domain}"
-  tags = ["ssh","http"]
-  
+  zone         = "us-central1-a"
+
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+      image = "debian-cloud/debian-9"
     }
   }
-  metadata_startup_script = "sudo apt-get update && sudo apt-get install -yq build-essential apache2"
+
   network_interface {
-    network = google_compute_network.vpc.name
-    subnetwork = google_compute_subnetwork.public_subnet_1.name
-  
-  access_config { }
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
   }
+
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+    // Apply the firewall rule to allow external IPs to access this instance
+    tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http-terraform"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
