c8e3bfc3ef647ec5d405a4b3f0c7c9d182610239
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-07-11 15:54:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
-# setup the GCP provider
-terraform {
-  required_version = ">= 0.12"
-}
 provider "google" {
-  project     = "dolphine-project"
-  credentials = file("/home/sravangcp/gcp-key.json")
-  region      = "europe-west1"
-  zone        = "europe-west1-b"
+credentials = "${file("~/gcp-key.json")}"
+project = "dolphine-project"
+region = "us-central1"
 }
